53242181fe98ca53fa9985424f403ef38f0e4f4c
daiyan1998 <abduddaiyan7@gmail.com>
2023-05-18 03:27:36 +0600

    added gallary section

---
+++ src/pages/Home/Home/Home.jsx
@@ -0,0 +1,13 @@
+import Banner from "../Banner/Banner";
+import Gallary from "../Gallary/Gallary";
+
+const Home = () => {
+  return (
+    <>
+      <Banner></Banner>
+      <Gallary></Gallary>
+    </>
+  );
+};
+
+export default Home;
